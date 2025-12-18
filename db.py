ecf3a46f7ea35118774bbc96543c51e93ba2e87f
BiniyamGeremew <biniyamgeremew42@gmail.com>
2025-12-18 05:28:59 -1200

    used env variable for the db url

--- db.py
+++ db.py
@@ -1,25 +1,23 @@
+import os
 import psycopg2
-from config import DATABASE_URL 
 
-# Connect to PostgreSQL
+DATABASE_URL = os.environ.get("DATABASE_URL")  # get from env variables
+
 conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
 
-# Function to save user
 def save_user(user):
     user_id = str(user.id)
     username = user.username or ""
-
     cursor.execute("""
         INSERT INTO users (user_id, username)
         VALUES (%s, %s)
         ON CONFLICT (user_id) DO UPDATE
         SET username = EXCLUDED.username;
     """, (user_id, username))
-
     conn.commit()
 
-# Function to get total users
 def get_total_users():
     cursor.execute("SELECT COUNT(*) FROM users;")
-    return cursor.fetchone()[0]
+    result = cursor.fetchone()
+    return result[0] if result else 0
